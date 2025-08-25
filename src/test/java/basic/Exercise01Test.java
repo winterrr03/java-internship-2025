3f4b250f7b98359b22d5e82ad02a1de2f9c79ec1
winterrr03 <dong.huynhquang21503@gmail.com>
2025-08-25 21:44:19 +0700

    Add basic exercise 01

---
+++ src/test/java/basic/Exercise01Test.java
@@ -0,0 +1,29 @@
+package basic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Exercise01Test {
+
+    private final Exercise01 exercise01 = new Exercise01();
+
+    @Test
+    void getGCD() {
+        assertEquals(0, exercise01.getGCD(0, 0));
+        assertEquals(5, exercise01.getGCD(5, 0));
+        assertEquals(10, exercise01.getGCD(0, 10));
+        assertEquals(6, exercise01.getGCD(12, 18));
+        assertEquals(1, exercise01.getGCD(7, 13));
+    }
+
+    @Test
+    void getLCM() {
+        assertEquals(0, exercise01.getLCM(0, 5));
+        assertEquals(0, exercise01.getLCM(7, 0));
+        assertEquals(0, exercise01.getLCM(0, 0));
+        assertEquals(12, exercise01.getLCM(4, 6));
+        assertEquals(91, exercise01.getLCM(7, 13));
+        assertEquals(10, exercise01.getLCM(5, 10));
+    }
+}
