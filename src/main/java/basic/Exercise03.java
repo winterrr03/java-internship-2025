c9fb4347d75c5911103e6cc63a4c710e1a3fc9c2
winterrr03 <dong.huynhquang21503@gmail.com>
2025-08-26 09:57:38 +0700

    Add final to parameter

--- src/main/java/basic/Exercise03.java
+++ src/main/java/basic/Exercise03.java
@@ -1,24 +1,25 @@
 package basic;
 
 public class Exercise03 {
-    public String factorizePrime(int n) {
+    public String factorizePrime(final int n) {
         final StringBuilder result = new StringBuilder();
 
-        for (int i = 2; i * i <= n; i++) {
-            while (n % i == 0) {
+        int value = n;
+        for (int i = 2; i * i <= value; i++) {
+            while (value % i == 0) {
                 if (result.length() != 0) {
                     result.append(" * ");
                 }
                 result.append(i);
-                n /= i;
+                value /= i;
             }
         }
 
-        if (n > 1) {
+        if (value > 1) {
             if (result.length() != 0) {
                 result.append(" * ");
             }
-            result.append(n);
+            result.append(value);
         }
 
         return result.toString();
