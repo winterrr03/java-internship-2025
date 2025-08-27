7b04721a9d911c9633e5e9242ec06acc3f435eeb
winterrr03 <dong.huynhquang21503@gmail.com>
2025-08-27 13:53:47 +0700

    Add array exercise 01

---
+++ src/main/java/array/Exercise01.java
@@ -0,0 +1,22 @@
+package array;
+
+public class Exercise01 {
+    public int sumEvenSubtractOdd(int[] numbers) {
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+
+        for (int n : numbers) {
+            if (n % 2 == 0) {
+                result += n;
+                continue;
+            }
+
+            result -= n;
+        }
+
+        return result;
+    }
+}
